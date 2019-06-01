fd388389816ad1bf3a9ebd1f20720d187886bdd0
Paulo Gomes <pauloamgomes@gmail.com>
2019-06-01 09:27:50 +0100

    included actions also on REST and CLI commands

--- bootstrap.php
+++ bootstrap.php
@@ -562,11 +562,13 @@ if (COCKPIT_ADMIN && !COCKPIT_API_REQUEST) {
 
 // CLI includes.
 if (COCKPIT_CLI) {
+  include_once __DIR__ . '/actions.php';
   $this->path('#cli', __DIR__ . '/cli');
 }
 
 // If REST include handlers for remote style actions.
 if (COCKPIT_API_REQUEST) {
+  include_once __DIR__ . '/actions.php';
   $this->on('cockpit.rest.init', function ($routes) {
       $routes['imagestyles'] = 'ImageStyles\\Controller\\RestApi';
   });
